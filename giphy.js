96f4f036966e9ef61feefb633ed3de89a12dc5a0
Gowrignaneswarreddy <gowrignaneswarreddy@gmail.com>
2024-10-16 22:03:47 +0530

    gifhy

---
+++ giphy.js
@@ -0,0 +1,29 @@
+const apiKey = 'cKv8C11U8yC0xGd8uXGNAPRwd7ijDMN4'; // Replace with your Giphy API key
+
+        function searchGifs() {
+            const searchTerm = document.getElementById('search').value;
+            const url =(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}&limit=10&offset=0&rating=g&lang=en`);
+
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    
+                    const gifContainer = document.getElementById('gif-container');
+                    gifContainer.innerHTML = ''; // Clear previous results
+
+                    data.data.forEach(gif => {
+                        const gifDiv = document.createElement('div');
+                        gifDiv.classList.add('gif');
+
+                        const gifImg = document.createElement('img');
+                        gifImg.src = gif.images.original.url;
+
+                        gifDiv.appendChild(gifImg);
+                        gifContainer.appendChild(gifDiv);
+                    });
+                })
+                .catch(error => {
+                    console.error('Error fetching GIFs:', error);
+                });
+        }
